8067d17c30db1d8fda736a058279e77de01e0857
Mattias Hellman <mattias.hellman@gmail.com>
2026-03-12 19:02:59 +0100

    Change order list margins

--- src/components/CartModal.jsx
+++ src/components/CartModal.jsx
@@ -12,7 +12,7 @@ const CartModal = ({orders, foodItems, onChangeQuantity, onClearEmptyOrders}) =>
             <button type="button" className="btn-close ms-0" data-bs-dismiss="modal" aria-label="Close"></button>
           </div>
           <div className="modal-body d-flex flex-column">
-            <h2 className="fs-5 text-start">Order details</h2>
+            <h2 className="fs-5 text-start mb-3">Order details</h2>
             {
               orders.length == 0 ? 
                 <div className="text-center my-5 fs-5">Your cart is empty</div>
@@ -20,13 +20,13 @@ const CartModal = ({orders, foodItems, onChangeQuantity, onClearEmptyOrders}) =>
                 orders.map((order) => {
                   const foodItem = foodItems.find((f) => f.id === order.foodId);
                   return (
-                    <div key={order.id} className="d-flex align-items-center justify-content-between my-3">
+                    <div key={order.id} className="d-flex align-items-center justify-content-between my-2">
                       <OrderItem id={order.id} quantity={order.quantity} name={foodItem?.name} price={foodItem?.price} onChangeQuantity={onChangeQuantity} /> 
                     </div>
                   )
                 })
             }
-            <div className="text-end mt-auto">
+            <div className="text-end mt-auto pt-3">
               Total: SEK
               <span className="ms-3 fw-bold">
                 {orders.reduce((accumulator, order) => {
