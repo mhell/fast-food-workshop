0c1af1479fcb0875c5b25515ac52924cc5329cbc
Mattias Hellman <mattias.hellman@gmail.com>
2026-03-13 10:48:09 +0100

    Fix orderlist quantity alignment

--- src/components/OrderItem.jsx
+++ src/components/OrderItem.jsx
@@ -8,7 +8,7 @@ const OrderItem = ({id, quantity, name, price, onChangeQuantity}) => {
         <button type="button" className="btn btn-light" onClick={() => onChangeQuantity(id, -1)}>
           <i className="bi bi-dash"></i>
         </button>
-        <span className="mx-3">{quantity}</span>
+        <div className="d-inline-block" style={{width: "3rem"}}>{quantity}</div>
         <button type="button" className="btn btn-light"  onClick={() => onChangeQuantity(id, +1)}>
           <i className="bi bi-plus"></i>
         </button>
